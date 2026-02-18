e0e17b390ad62b70703dfa391f23ab7d34ebee6f
Mugagga <Mugagga.kimera04@gmail.com>
2026-02-18 14:43:55 +0000

    Added process ID in logger base configuration

--- services/authentication/src/config/logger.ts
+++ services/authentication/src/config/logger.ts
@@ -1,9 +1,10 @@
 import { hostname } from "os";
 import pino from "pino";
+import { pid } from "process";
 
 const logger = pino({
     level: process.env.LOG_LEVEL || "info",
-    base: { service: "authentication-service", hostname: hostname() },
+    base: { service: "authentication-service", hostname: hostname(), pid: pid },
     timestamp: pino.stdTimeFunctions.isoTime,
 })
 
