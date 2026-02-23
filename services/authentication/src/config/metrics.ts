fa2a3ae65833fafb93d7c8e826b7de5a91f64b7b
Mugagga <Mugagga.kimera04@gmail.com>
2026-02-23 10:29:04 +0000

    Adding basic testing and logging with prometheus and loki

--- services/authentication/src/config/metrics.ts
+++ services/authentication/src/config/metrics.ts
@@ -3,3 +3,23 @@
 // mainly prometheus metrics and opentelemetry traces and logs
 
 // were going to have ping , current system status , might also try to track requests and faliures and response times for the endpoints                                                                      
+import client from "prom-client"
+
+export const register = new client.Registry()
+
+register.setDefaultLabels({
+    app: "authentication_service"
+})
+
+export const requestDurationHistogram = new client.Histogram({
+    name: "request_duration_seconds",
+    help: "Duration of requests in seconds",
+    labelNames: ["method", "route", "status_code"],
+    buckets: [0.1, 0.5, 1, 2.5, 5, 10],
+    registers: [register]
+})
+
+
+
+client.collectDefaultMetrics({ register })
+
