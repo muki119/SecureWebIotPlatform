26619d5ad0e6cc2182153f63235e8b3bbc12792b
Mugagga <Mugagga.kimera04@gmail.com>
2026-03-15 02:29:38 +0000

    capitalised MetricsController name to conform with naming conventions

--- services/authentication/src/controllers/metrics_controller.ts
+++ services/authentication/src/controllers/metrics_controller.ts
@@ -2,7 +2,7 @@ import { register } from "../config/metrics";
 import type { Request, Response } from 'express';
 
 
-export default async function metricsController(req: Request, res: Response) {
+export default async function MetricsController(req: Request, res: Response) {
     res.set('Content-Type', register.contentType);
     res.end(await register.metrics());
 }
