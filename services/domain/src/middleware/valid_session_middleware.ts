a2b3f437b5acfd6428514068a5178d62a45cfe4c
Mugagga <Mugagga.kimera04@gmail.com>
2026-03-23 04:20:12 +0000

    added session middleware to domain and profile routes

---
+++ services/domain/src/middleware/valid_session_middleware.ts
@@ -0,0 +1,4 @@
+import { SessionMiddleware } from '@services/common/middleware';
+import logger from '../config/logger';
+
+export const ValidSessionMiddleware = new SessionMiddleware(logger).middleware;
--- services/domain/src/routes/index.ts
+++ services/domain/src/routes/index.ts
@@ -1,11 +1,13 @@
 import DomainRouter from "./domain_routes";
-import ProfileRouter from "./profile_routes";
+import ProfileRouter from "./profile_routes"
+import { ValidSessionMiddleware } from "../middleware/valid_session_middleware";
 import express from 'express';
 
 
 
 const Domain_ProfileRouter = express.Router();
 
+Domain_ProfileRouter.use(ValidSessionMiddleware);
 Domain_ProfileRouter.use("/domain", DomainRouter);
 Domain_ProfileRouter.use("/profile", ProfileRouter);
 
