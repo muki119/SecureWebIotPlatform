ed74cf2f3352fae0395180b2960d6a0dee7a91ea
Mugagga <Mugagga.kimera04@gmail.com>
2026-03-23 04:21:55 +0000

    Added service result comments

--- services/common/types/service.d.ts
+++ services/common/types/service.d.ts
@@ -2,5 +2,7 @@
 * * if theres something truly exceptional , i think it only makes sense to throw an error
 * * otherwise if its just a logical non killer error - like user not found then it makes sense to return service result
 * * this way it kinda acts a dual chanel error handling - you handle the truly ecceptional and the mundane in clear ways and you know where they go and where to put it.
+* 
+* * Service results are usually errors caused by problems with the request data - so the res will mosst likely be a 40x error
 */
 export type ServiceResult<T, E = Error> = [T, null] | [null, E extends Error ? E : Error] // Go style tuple result , because every other return type is annoying 
