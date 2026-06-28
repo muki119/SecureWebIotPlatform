1bb747a92d1379b6fbb99b21d8c275acbf4ac53a
Mugagga <Mugagga.kimera04@gmail.com>
2026-06-28 04:32:37 +0100

    Added domain deletion topic for mqtt

--- services/devicecontrol/src/constants/mqtt_topics.ts
+++ services/devicecontrol/src/constants/mqtt_topics.ts
@@ -6,6 +6,11 @@ export const MQTT_TOPICS = {
         },
         UNLINKED(deviceId: string) {
             return `/device/${deviceId}/unlinked`
+        },
+        DOMAIN(domainId: string) {
+            return {
+                DELETED: `/domain/${domainId}/deleted`
+            } as const
         }
     },
     DEVICE_EMITTED: {
--- services/devicecontrol/src/constants/socket_events.ts
+++ services/devicecontrol/src/constants/socket_events.ts
@@ -1,6 +1,3 @@
-
-
-
 export const SOCKET_EVENTS = {
 
     CLIENT_EMITTED: {
